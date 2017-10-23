783fbc6b50fa621f9b3aefef48dddd2be2cdc497
Anand Balakrishnan <anandbala1597@gmail.com>
2017-10-23 17:22:22 -0400

    Initial commit

---
+++ terrain.tsx
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="terrain" tilewidth="32" tileheight="32" tilecount="483" columns="21">
+ <image source="assets/terrain.png" width="672" height="736"/>
+ <terraintypes>
+  <terrain name="Water" tile="322"/>
+  <terrain name="Grass" tile="193"/>
+  <terrain name="Sand" tile="319"/>
+  <terrain name="Earth" tile="208"/>
+ </terraintypes>
+ <tile id="123" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="124" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="125" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="127" terrain="1,1,1,"/>
+ <tile id="128" terrain="1,1,,1"/>
+ <tile id="130" terrain="1,1,1,"/>
+ <tile id="131" terrain="1,1,,1"/>
+ <tile id="145" terrain="3,3,3,1"/>
+ <tile id="146" terrain="3,3,1,3"/>
+ <tile id="148" terrain="1,,1,1"/>
+ <tile id="149" terrain=",1,1,1"/>
+ <tile id="151" terrain="1,,1,1"/>
+ <tile id="152" terrain=",1,1,1"/>
+ <tile id="166" terrain="3,1,3,3"/>
+ <tile id="167" terrain="1,3,3,3"/>
+ <tile id="168" terrain=",,,1"/>
+ <tile id="169" terrain=",,1,1"/>
+ <tile id="170" terrain=",,1,"/>
+ <tile id="171" terrain=",,,1"/>
+ <tile id="172" terrain=",,1,1"/>
+ <tile id="173" terrain=",,1,"/>
+ <tile id="186" terrain="1,1,1,3"/>
+ <tile id="187" terrain="1,1,3,3"/>
+ <tile id="188" terrain="1,1,3,1"/>
+ <tile id="189" terrain=",1,,1"/>
+ <tile id="190" terrain="1,1,1,1"/>
+ <tile id="191" terrain="1,,1,"/>
+ <tile id="192" terrain=",1,,1"/>
+ <tile id="193" terrain="1,1,1,1"/>
+ <tile id="194" terrain="1,,1,"/>
+ <tile id="207" terrain="1,3,1,3"/>
+ <tile id="208" terrain="3,3,3,3"/>
+ <tile id="209" terrain="3,1,3,1"/>
+ <tile id="210" terrain=",1,,"/>
+ <tile id="211" terrain="1,1,,"/>
+ <tile id="212" terrain="1,,,"/>
+ <tile id="213" terrain=",1,,"/>
+ <tile id="214" terrain="1,1,,"/>
+ <tile id="215" terrain="1,,,"/>
+ <tile id="220" terrain=",,0,"/>
+ <tile id="228" terrain="1,3,1,1"/>
+ <tile id="229" terrain="3,3,1,1"/>
+ <tile id="230" terrain="3,1,1,1"/>
+ <tile id="231" terrain="1,1,1,1" probability="0.35"/>
+ <tile id="232" terrain="1,1,1,1" probability="0.35"/>
+ <tile id="233" terrain="1,1,1,1" probability="0.35"/>
+ <tile id="234" terrain="1,1,1,1" probability="0.35"/>
+ <tile id="235" terrain="1,1,1,1" probability="0.35"/>
+ <tile id="236" terrain="1,1,1,1" probability="0.35"/>
+ <tile id="253" terrain="2,2,2,0"/>
+ <tile id="254" terrain="2,2,0,2"/>
+ <tile id="256" terrain="2,2,2,0"/>
+ <tile id="257" terrain="2,2,0,2"/>
+ <tile id="259" terrain="0,0,0,1"/>
+ <tile id="260" terrain="0,0,1,0"/>
+ <tile id="274" terrain="2,0,2,2"/>
+ <tile id="275" terrain="0,2,2,2"/>
+ <tile id="277" terrain="2,0,2,2"/>
+ <tile id="278" terrain="0,2,2,2"/>
+ <tile id="280" terrain="0,1,0,0"/>
+ <tile id="281" terrain="1,0,0,0"/>
+ <tile id="294" terrain="0,0,0,2"/>
+ <tile id="295" terrain="0,0,2,2"/>
+ <tile id="296" terrain="0,0,2,0"/>
+ <tile id="297" terrain="0,0,0,2"/>
+ <tile id="298" terrain="0,0,2,2"/>
+ <tile id="299" terrain="0,0,2,0"/>
+ <tile id="300" terrain="1,1,1,0"/>
+ <tile id="301" terrain="1,1,0,0"/>
+ <tile id="302" terrain="1,1,0,1"/>
+ <tile id="315" terrain="0,2,0,2"/>
+ <tile id="316" terrain="2,2,2,2"/>
+ <tile id="317" terrain="2,0,2,0"/>
+ <tile id="318" terrain="0,2,0,2"/>
+ <tile id="319" terrain="2,2,2,2"/>
+ <tile id="320" terrain="2,0,2,0"/>
+ <tile id="321" terrain="1,0,1,0"/>
+ <tile id="322" terrain="0,0,0,0"/>
+ <tile id="323" terrain="0,1,0,1"/>
+ <tile id="336" terrain="0,2,0,0"/>
+ <tile id="337" terrain="2,2,0,0"/>
+ <tile id="338" terrain="2,0,0,0"/>
+ <tile id="339" terrain="0,2,0,0"/>
+ <tile id="340" terrain="2,2,0,0"/>
+ <tile id="341" terrain="2,0,0,0"/>
+ <tile id="342" terrain="1,0,1,1"/>
+ <tile id="343" terrain="0,0,1,1"/>
+ <tile id="344" terrain="0,1,1,1"/>
+ <tile id="357" terrain="2,2,2,2"/>
+ <tile id="358" terrain="2,2,2,2"/>
+ <tile id="359" terrain="2,2,2,2"/>
+ <tile id="360" terrain="0,0,0,0" probability="0.2"/>
+ <tile id="361" terrain="0,0,0,0" probability="0.2"/>
+ <tile id="362" terrain="0,0,0,0" probability="0.2"/>
+ <tile id="363" terrain="0,0,0,0" probability="0.2"/>
+ <tile id="364" terrain="0,0,0,0" probability="0.2"/>
+ <tile id="365" terrain="0,0,0,0"/>
+</tileset>
