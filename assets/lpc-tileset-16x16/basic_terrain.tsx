bea349f700910023ba74b4ffe4ce20d48dc0bb11
Sriram Subramanian <subrams6@gmail.com>
2017-10-31 02:06:37 -0400

    added correct tileset and modified map to be larger and added staircase

---
+++ assets/lpc-tileset-16x16/basic_terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Basic_terrain" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="105" columns="5">
+ <image source="lpc-tileset-16x16/tileset_basic_terrain.png" width="192" height="704"/>
+</tileset>
