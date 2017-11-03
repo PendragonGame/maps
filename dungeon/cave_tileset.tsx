582748ce78dbe0c4e523dba697a883f67c78c5be
Anand Balakrishnan <anandbala1597@gmail.com>
2017-11-03 15:25:56 -0400

    Added teleport properties

--- dungeon/cave_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="cave2" tilewidth="16" tileheight="16" tilecount="1739" columns="47">
- <image source="../../../Desktop/lpc-tileset-16x16/tileset_cave.png" width="752" height="592"/>
-</tileset>
